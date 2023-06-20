ae9a7a30a1a24c7d1311e4930a2a16d796241993
Sarim Khan <sarimk360@gmail.com>
2023-06-20 21:31:51 +0530

    new commit
    made few changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { QuizAppComponent } from './quiz-app/quiz-app.component';
-import { QuestionComponent } from './question/question.component';
-import { AnswersComponent } from './answers/answers.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizAppComponent,
-    QuestionComponent,
-    AnswersComponent,
-    FooterComponent
+    QuizAppComponent
   ],
   imports: [
     BrowserModule
